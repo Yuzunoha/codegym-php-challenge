baa21d080e0e81810f30f7a83f1bbf23d6db854e
Yuzunoha <studysteadystandby@gmail.com>
2022-03-22 12:37:32 +0900

    自動修正

--- html/index.php
+++ html/index.php
@@ -51,37 +51,38 @@ $tweet_count = count($tweets);
 <?php require_once('head.php'); ?>
 
 <body>
-  <div class="container">
-    <h1 class="my-5">新規投稿</h1>
-    <div class="card mb-3">
-      <div class="card-body">
-        <form method="POST">
-          <textarea class="form-control" type=textarea name="tweet_textarea" ?><!-- 返信課題はここを修正しましょう。 --></textarea>
-          <!-- 返信課題はここからのコードを修正しましょう。 -->
-          <!-- 返信課題はここからのコードを修正しましょう。 -->
-          <br>
-          <input class="btn btn-primary" type=submit value="投稿">
-        </form>
-      </div>
-    </div>
-    <h1 class="my-5">コメント一覧</h1>
-    <?php foreach ($tweets as $t) { ?>
-      <div class="card mb-3">
-        <div class="card-body">
-          <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
-          <p class="card-text"><?= "{$t['text']}" ?></p>
-          <!--返信課題はここから修正しましょう。-->
-          <!--<p>[返信する] [返信元のメッセージ]</p>-->
-          <!--返信課題はここまで修正しましょう。-->
+    <div class="container">
+        <h1 class="my-5">新規投稿</h1>
+        <div class="card mb-3">
+            <div class="card-body">
+                <form method="POST">
+                    <textarea class="form-control" type=textarea name="tweet_textarea" ?><!-- 返信課題はここを修正しましょう。 --></textarea>
+                    <!-- 返信課題はここからのコードを修正しましょう。 -->
+                    <!-- 返信課題はここからのコードを修正しましょう。 -->
+                    <br>
+                    <input class="btn btn-primary" type=submit value="投稿">
+                </form>
+            </div>
         </div>
-      </div>
-    <?php } ?>
-    <form method="POST">
-      <input type="hidden" name="logout" value="dummy">
-      <button class="btn btn-primary">ログアウト</button>
-    </form>
-    <br>
-  </div>
+        <h1 class="my-5">コメント一覧</h1>
+        <?php foreach ($tweets as $t) { ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
+                    <p class="card-text"><?= "{$t['text']}" ?></p>
+                    <!--返信課題はここから修正しましょう。-->
+                    <p>[返信する] <a href="/view.php?id=<?= "{$t['reply_id']}" ?>">[返信元のメッセージ]</a></p>
+                    <!--<p>[返信する] [返信元のメッセージ]</p>-->
+                    <!--返信課題はここまで修正しましょう。-->
+                </div>
+            </div>
+        <?php } ?>
+        <form method="POST">
+            <input type="hidden" name="logout" value="dummy">
+            <button class="btn btn-primary">ログアウト</button>
+        </form>
+        <br>
+    </div>
 </body>
 
 </html>
